6e3f973026ee76177e047c1af241edcd20aacc57
Michael Nelson <michaelnelson07@gmail.com>
2016-02-08 17:20:04 +0000

    gulpfile.js: New file
    This will help facilitate minifying and concatenating our CSS.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	minify = require('gulp-minify-css')
+
+// helps keep the file order
+files = [
+	'./css/naut.css',
+	'./css/fcdallas.css'
+]
+
+gulp.task('minify', function() {
+	gulp.src(files)
+		.pipe(concat('minified.css'))
+		.pipe(minify())
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['minify'])
