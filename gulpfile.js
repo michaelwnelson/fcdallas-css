62d10a3e0d660994f13b948c2fca1acc19c90e15
Michael Nelson <michaelnelson07@gmail.com>
2018-03-07 19:44:54 -0600

    gulfile: Change spacing
    Started using Prettier, indentation with spaces, indentation width 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat'),
-	minify = require('gulp-clean-css')
+  concat = require('gulp-concat'),
+  minify = require('gulp-clean-css'),
+  gutil = require('gulp-util');
 
 // helps keep the file order
-files = [
-	'./css/naut.css',
-	'./css/fcdallas.css'
-]
+files = ['./css/naut.css', './css/fcdallas.css'];
 
 gulp.task('minify', function() {
-	gulp.src(files)
-		.pipe(concat('minified.css'))
-		.pipe(minify())
-		.pipe(gulp.dest('./'));
+  gulp
+    .src(files)
+    .pipe(concat('minified.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('./'));
+
+  return gutil.log('minified.css generated!');
 });
 
-gulp.task('default', ['minify'])
+gulp.task('default', ['minify']);
